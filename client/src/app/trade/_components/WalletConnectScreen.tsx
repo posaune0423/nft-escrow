d5eae7a9acd684cc0bb856b2abf4f64294fdc50d
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-12 06:49:07 -0400

    fix: button height

--- client/src/app/trade/_components/WalletConnectScreen.tsx
+++ client/src/app/trade/_components/WalletConnectScreen.tsx
@@ -7,7 +7,7 @@ export const WalletConnectScreen = () => {
     <main className="flex flex-col justify-between min-h-[calc(100dvh-180px)] py-8 max-w-md mx-auto px-4">
       <h2 className="text-2xl font-bold">ウォレットを接続してください</h2>
 
-      <Button onClick={openConnectModal} className="w-full h-16 text-lg rounded-3xl">
+      <Button onClick={openConnectModal} className="w-full h-14 md:h-16 text-lg rounded-3xl">
         Walletを接続
       </Button>
     </main>
