8cf426dca01c9e615fb1f63cdadad41176637bbc
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-12 06:26:37 -0400

    fix: space and font size

--- client/src/components/LP.tsx
+++ client/src/components/LP.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 export const LP = () => {
   return (
     <main className="flex flex-col justify-between p-4 max-w-md mx-auto min-h-[calc(100dvh-180px)] space-y-6 md:space-y-2">
-      <h1 className="text-6xl font-bold text-primary font-delaGothicOne mx-auto">{APP_NAME}</h1>
+      <h1 className="text-5xl md:text-6xl font-bold text-primary font-delaGothicOne mx-auto">{APP_NAME}</h1>
 
-      <div className="bg-white text-slate-900 px-8 py-10 rounded-lg shadow-xl border">
+      <div className="bg-white text-slate-900 px-6 md:px-8 py-8 md:py-10 rounded-lg shadow-xl border">
         <h2 className="text-xl md:text-2xl font-bold mb-6">NFT・トークンを安全に交換</h2>
         <p className="text-sm md:text-lg text-muted-foreground mb-4 px-2">
           エスクローでは、NFTやトークンの個人間取引を安全かつ簡単に行うことができます。
@@ -20,7 +20,7 @@ export const LP = () => {
         <p className="text-lg md:text-xl font-semibold">スムーズで安心な個人間取引を。</p>
       </div>
 
-      <Button className="w-full h-16 text-lg rounded-3xl" size="lg">
+      <Button className="w-full h-15 text-md rounded-3xl" size="lg">
         <Link href="/trade">交換を始める 👈🏻</Link>
       </Button>
     </main>
