08c5a4c0528ba3a130b0ff53dd57e261f2ba9e01
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-12 06:46:59 -0400

    fix: ui

--- client/src/app/trade/_components/WalletConnectScreen.tsx
+++ client/src/app/trade/_components/WalletConnectScreen.tsx
@@ -4,13 +4,12 @@ import { useConnectModal } from "@rainbow-me/rainbowkit";
 export const WalletConnectScreen = () => {
   const { openConnectModal } = useConnectModal();
   return (
-    <main className="flex flex-col min-h-[calc(100dvh-180px)] py-8 max-w-md mx-auto">
-      <div className="flex flex-col items-center justify-center px-4 space-y-4">
-        <h2 className="text-2xl font-bold">ウォレットを接続してください</h2>
-        <Button onClick={openConnectModal} className="w-full h-16 text-lg rounded-3xl">
-          Walletを接続
-        </Button>
-      </div>
+    <main className="flex flex-col justify-between min-h-[calc(100dvh-180px)] py-8 max-w-md mx-auto px-4">
+      <h2 className="text-2xl font-bold">ウォレットを接続してください</h2>
+
+      <Button onClick={openConnectModal} className="w-full h-16 text-lg rounded-3xl">
+        Walletを接続
+      </Button>
     </main>
   );
 };
